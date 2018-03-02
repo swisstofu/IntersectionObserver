45072d202dea1a31d381c300a30a7a09bc74b7d1
Christophe Krebser <christophe.krebser@leshop.ch>
2018-03-02 17:06:12 +0100

    moving npm package to root

--- intersection-observer.js
+++ intersection-observer.js
@@ -16,19 +16,18 @@
 if ('IntersectionObserver' in window &&
     'IntersectionObserverEntry' in window &&
     'intersectionRatio' in window.IntersectionObserverEntry.prototype) {
-
-  // Minimal polyfill for Edge 15's lack of `isIntersecting`
-  // See: https://github.com/w3c/IntersectionObserver/issues/211
-  if (!('isIntersecting' in window.IntersectionObserverEntry.prototype)) {
-    Object.defineProperty(window.IntersectionObserverEntry.prototype,
-      'isIntersecting', {
-      get: function () {
-        return this.intersectionRatio > 0;
-      }
-    });
-  } else {
+  if ('isIntersecting' in window.IntersectionObserverEntry.prototype) {
     return;
   }
+  // CKR: Use polyfill for Edge 40 On Windows Phone
+  // Minimal polyfill for Edge 15's lack of `isIntersecting`
+  // See: https://github.com/w3c/IntersectionObserver/issues/211
+  Object.defineProperty(window.IntersectionObserverEntry.prototype,
+    'isIntersecting', {
+    get: function () {
+      return this.intersectionRatio > 0;
+    }
+  });
 }
 
 
